9471ff62c9ac3b199a7c78b9364f5be26c9fefd2
gaurav dalakoti <gauravdalakoti046@gmail.com>
2024-09-24 22:45:17 +0530

    UPDATE CORS ORIGIN

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.set('trust proxy', 1);
 
-app.use(cors({ credentials: true, methods: ["POST", "GET", "PATCH"], origin: process.env.CORS_ORIGIN }))
+app.use(cors({ credentials: true, methods: ["POST", "GET", "PATCH","DELETE"], origin: process.env.CORS_ORIGIN }))
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
